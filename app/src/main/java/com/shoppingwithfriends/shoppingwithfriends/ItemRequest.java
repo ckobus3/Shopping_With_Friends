eae91ad248c3c90b0d4c98f514507e113bf54f74
Colby McBride <colbym84@gmail.com>
2015-02-22 16:17:01 -0500

    updated database version

---
+++ app/src/main/java/com/shoppingwithfriends/shoppingwithfriends/ItemRequest.java
@@ -0,0 +1,63 @@
+package com.shoppingwithfriends.shoppingwithfriends;
+
+
+public class ItemRequest {
+
+    private int requestId;
+    private User requester;
+    private String name;
+    private int maxPrice;
+    private String location;
+
+    public ItemRequest() {
+    }
+
+    public ItemRequest(User user, String name, int price, String location) {
+        this.requester = user;
+        this.name = name;
+        this.maxPrice = price;
+        this.location = location;
+    }
+
+    public int getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(int id) {
+        this.requestId = id;
+    }
+
+    public User getRequester() {
+        return requester;
+    }
+
+    public void setRequester(User user) {
+        this.requester = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxPrice() {
+        return maxPrice;
+    }
+
+    public void setMaxPrice(int price) {
+        this.maxPrice = price;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String loc) {
+        this.location = loc;
+    }
+
+
+}
